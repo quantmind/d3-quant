0118e8ff3edecd42034cde1081e1ca8436430472
Luca Sbardella <luca@quantmind.com>
2020-07-08 12:48:28 +0100

    Release 0.5.0

--- tests/btree.test.ts
+++ tests/btree.test.ts
@@ -3,6 +3,7 @@ import { binaryTree } from "../src";
 describe("Btree", () => {
   test("test btree constructor", () => {
     const tree = binaryTree();
+    expect(tree.score).toBe(0);
     expect(tree.size).toBe(0);
     expect(tree.depth).toBe(0);
     expect(tree.maxDepth).toBe(0);
@@ -13,6 +14,7 @@ describe("Btree", () => {
     tree.insert(0.5);
     expect(tree.size).toBe(1);
     expect(tree.maxDepth).toBe(0);
+    expect(tree.score).toBe(0.5);
 
     tree.insert(0.2);
     expect(tree.size).toBe(2);
@@ -21,7 +23,7 @@ describe("Btree", () => {
     expect(tree.size).toBe(3);
     expect(tree.maxDepth).toBe(1);
 
-    expect(tree.root?.score).toBe(0.3);
+    expect(tree.score).toBe(0.3);
 
     const nodes = tree.nodes();
     expect(nodes.length).toBe(3);
